76fca2ebc539013713db5a3192d1fafb12c5607f
eszterk90git config --global user.email eszter.kadar1@gmail.com <82053220+eszterk90@users.noreply.github.com>
2022-06-27 13:27:14 +0200

    signUp signIn component created

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
 import { getAnalytics } from "firebase/analytics";
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
 
 
 // Your web app's Firebase configuration
@@ -18,3 +20,63 @@ const firebaseConfig = {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+
+  const provider = new GoogleAuthProvider();
+  provider.setCustomParameters({
+    prompt: 'select_account'
+  });
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
+
+// instantiate our database:
+export const db = getFirestore();
+
+// async function that receives a user documentation object from logGoogleUser:
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+    if (!userAuth) return;
+    // check if there is a user document reference. 
+    // userDocRef = doc() - that takes three arguments: first: database, second: collections, third: identifier. 
+    // after user authentication we get a response with the user object. we can use it to access the unique id and use it as identifier. (user = userAuth)
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    // to get the data related to a document
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot) // points to the same identifier as userDocRef - but this is a special object because it enables us to check if that object exists
+    console.log(userSnapshot.exists())
+
+    // if user data does not exist --> create / set document with the data from the userAuth in my collection
+
+    if(!userSnapshot.exists()) {
+        // destructure display name and email from the user object that I get back after log in
+        const {displayName, email} = userAuth;
+
+        // to keep track of when the users log in
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {displayName, email, createdAt, ...additionalInformation})
+        }
+        catch (error) {
+            console.log('error creating the user', error.message);
+        }
+          
+
+    }
+    // if user data exists --> return userDocRef
+    return userDocRef;
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
+}
