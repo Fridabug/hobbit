3553c7a307c8bb841aaffb0cb6ed2e0dcbfee29b
FrankoSaf <franko.safradin@gmail.com>
2022-06-30 16:10:59 +0200

    done

--- src/components/authentication/authentication.jsx
+++ src/components/authentication/authentication.jsx
@@ -18,11 +18,11 @@ const Authentication = () => {
         setToggle(!toggle)
     }
 
-    if(currentUser) {
-        window.location.replace('/home')
-      }
+    // if(currentUser) {
+    //     window.location.replace('/home')
+    //   }
 
-    console.log(toggle);
+    // console.log(toggle);
 
     return (
         <div className='authentication-container'>
