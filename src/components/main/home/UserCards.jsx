6d09b3b24db061bb9726729e197087e702d7de59
Fridabug <friederikespring@posteo.de>
2022-07-01 16:32:34 +0200

    styling

--- src/components/main/home/UserCards.jsx
+++ src/components/main/home/UserCards.jsx
@@ -10,7 +10,7 @@ function UserCards() {
   return (
     <div className='card-container'>
       {
-        sortedUsers.map((user, key) => {
+        sortedUsers?.filter((i) => i.email !== currentUser.email).map((user, key) => {
           return <Card key={key} name={user.displayName} imgUrl={user.userData.image} hobbies={user.userData.hobbies} >{user.displayName} </Card>
         })
       }
