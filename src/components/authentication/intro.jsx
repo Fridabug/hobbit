954d3e46ac16f1a9a804b5066672f5b73cd6e4fe
Fridabug <friederikespring@posteo.de>
2022-07-03 21:53:43 +0200

    sign in

--- src/components/authentication/intro.jsx
+++ src/components/authentication/intro.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Intro() {
   return (
-    <div>
+    <div className='intro'>
       <h1>Welcome to Hobbit</h1>
       <h2>Find people who share your interests, meet and make friends!</h2>
     </div>
